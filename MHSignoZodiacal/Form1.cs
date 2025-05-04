71faf2124d5128300662cb5709c8cfb4534bed11
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-04 18:11:52 -0400

    Algoritmo para llenar los meses del año actual en el combo 1

--- MHSignoZodiacal/Form1.cs
+++ MHSignoZodiacal/Form1.cs
@@ -18,6 +18,21 @@ namespace MHSignoZodiacal
 
             this.comboMes.SelectedIndex = 0;
             this.comboDia.SelectedIndex = 0;
+
+            SetMeses();
+        }
+
+        private void SetMeses()
+        {
+            string mesActual = String.Empty;
+
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                DateTime fecha = new DateTime(2025, mes, 1);
+                mesActual = fecha.ToString("MMMM");
+
+                this.comboMes.Items.Add(mesActual);
+            }
         }
     }
 }
