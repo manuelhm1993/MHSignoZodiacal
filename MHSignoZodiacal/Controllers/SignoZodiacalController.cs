15f6ed506c9477b8c7a813aac897b728bc8f2985
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-06 12:25:22 -0400

    Paquete Utilitarios, clase Common, en ella se encapsulan las funciones comunes que pueden ser reutilizadas por otras clases

--- MHSignoZodiacal/Controllers/SignoZodiacalController.cs
+++ MHSignoZodiacal/Controllers/SignoZodiacalController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 using MHSignoZodiacal.Views;
 using MHSignoZodiacal.Models;
+using MHSignoZodiacal.Utilitarios;
 
 namespace MHSignoZodiacal.Controllers
 {
@@ -29,7 +30,7 @@ namespace MHSignoZodiacal.Controllers
 
                 mesActual = fecha.ToString("MMMM");
 
-                MapMonthDays.Add(Capitalize(mesActual), GetDaysOfMonth(DateTime.Now.Year, mes));
+                MapMonthDays.Add(Common.Capitalize(mesActual), Common.GetDaysOfMonth(DateTime.Now.Year, mes));
             }
 
             FormSignoZodiacal._comboMes.Items.AddRange(MapMonthDays.Keys.ToArray());
@@ -42,26 +43,10 @@ namespace MHSignoZodiacal.Controllers
             FormSignoZodiacal._comboDia.Items.Clear();
 
             FormSignoZodiacal._comboDia.Items.Add(firstItem);
-            FormSignoZodiacal._comboDia.Items.AddRange(GetRange(MapMonthDays[month]));
+            FormSignoZodiacal._comboDia.Items.AddRange(Common.GetRange(MapMonthDays[month]));
 
             FormSignoZodiacal._comboDia.SelectedIndex = 0;
         }
-
-        private static Object[] GetRange(int range)
-        {
-            Object[] days = new Object[range];
-
-            for (int i = 1; i <= range; i++)
-            {
-                days[i - 1] = i;
-            }
-
-            return days;
-        }
-
-        private static string Capitalize(string word) => char.ToUpper(word[0]) + word.Substring(1);
-
-        private static int GetDaysOfMonth(int year, int month) => DateTime.DaysInMonth(year, month);
 
         public static SignoZodiacalView GetIGUSignoZodiacal() => new SignoZodiacalView();
         #endregion
---
+++ MHSignoZodiacal/Utilitarios/Common.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHSignoZodiacal.Utilitarios
+{
+    public static class Common
+    {
+        public static Object[] GetRange(int range)
+        {
+            Object[] days = new Object[range];
+
+            for (int i = 1; i <= range; i++)
+            {
+                days[i - 1] = i;
+            }
+
+            return days;
+        }
+
+        public static string Capitalize(string word) => char.ToUpper(word[0]) + word.Substring(1);
+
+        public static int GetDaysOfMonth(int year, int month) => DateTime.DaysInMonth(year, month);
+    }
+}
